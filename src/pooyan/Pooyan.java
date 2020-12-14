0bcb1a81b71cc4e0b76730d3a137f6763e7515e6
unknown <you@example.com>
2020-12-14 13:54:54 +0900

    Pooyan

--- src/pooyan/Pooyan.java
+++ src/pooyan/Pooyan.java
@@ -3,12 +3,26 @@ package pooyan;
 import java.awt.Color;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Pooyan extends JPanel {
-	
+
 	public Pooyan pooyan = this;
 	private final static String TAG = "Pooyan : ";
 	
-	
+	private JLabel laElevator, laAttackBow, laAttackPy, laAttackMeatPy;
+
+	private void init() {
+
+	}
+
+	private void setting() {
+
+	}
+
+	private void batch() {
+
+	}
 }
